db61647356c8151812cfdb6379143311372bc43c
dantheuber <dan.essig@gmail.com>
2017-05-09 22:55:53 -0700

    fix spaces

--- src/db.js
+++ src/db.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 import config from '../config/';
 export default callback => {
-	// connect to a database if needed, then pass it to `callback`:
 	mongoose.connect(config.mongoose.db, callback);
 }
